df8da1390d631ddb4d60439f696c7cfe25a2ce03
Nick Falcon <nick@lumoslabs.com>
2017-09-13 15:52:43 -0700

    Updates to Course 1 and 2.

---
+++ Lessons/Unity8Weeks/Week1-Lesson1B/Assets/Scripts/Mover/Jumper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GroundDetector))]
+public class Jumper : MonoBehaviour
+{
+    [Tooltip ("How much force we apply when we jump into the air. the higher it is, the higher we jump.")]
+    public float jumpImpulse = 10.0f;
+
+    [Tooltip( "How long we have to wait between jumps.")]
+    public float jumpDelay = 0.5f;
+
+    private float lastTimeJumped;
+
+    private GroundDetector groundDetector;
+
+    public void Awake() 
+    {
+        groundDetector = GetComponent<GroundDetector>();
+    }
+
+    //applies a single burst of velocity upwards - jump!
+    public void Jump()
+    {
+        float timeSinceJumped = Time.time - lastTimeJumped;
+
+        //only apply the velocity if we're currently standing on the ground
+        if ( timeSinceJumped >= jumpDelay && groundDetector.isOnGround )
+        {
+            GetComponent<Rigidbody2D>().velocity += new Vector2( 0.0f, jumpImpulse );
+            lastTimeJumped = Time.time;
+        }
+    }
+}
